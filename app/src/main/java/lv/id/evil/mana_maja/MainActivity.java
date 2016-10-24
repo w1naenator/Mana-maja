997c5e6c85d1a6e81ffd447373aad3b7bba536d9
Valdis Bogdāns <valdis.bogdans@gmail.com>
2016-10-24 21:57:06 +0300

    Add some classes
    Moka7

--- app/src/main/java/lv/id/evil/mana_maja/MainActivity.java
+++ app/src/main/java/lv/id/evil/mana_maja/MainActivity.java
@@ -5,14 +5,14 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import static android.app.PendingIntent.getActivity;
+import android.view.View;
+import android.widget.Button;
+import Moka7.*;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final S7Client Client = new S7Client();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +26,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+
+        final Button button = (Button) findViewById(R.id.button_connect);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.exit(0);
             }
         });
     }
@@ -53,4 +61,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
