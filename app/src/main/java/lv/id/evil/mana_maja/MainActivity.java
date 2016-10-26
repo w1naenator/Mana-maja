fec7cf23f16a1c453905509d0e10142020ca06a0
Valdis Bogdāns <valdis.bogdans@gmail.com>
2016-10-26 18:26:41 +0300

    Added some things

--- app/src/main/java/lv/id/evil/mana_maja/MainActivity.java
+++ app/src/main/java/lv/id/evil/mana_maja/MainActivity.java
@@ -10,18 +10,20 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.ProgressBar;
 
 import Moka7.S7;
 import Moka7.S7Client;
 
 public class MainActivity extends AppCompatActivity {
-    private static final S7Client Client = new S7Client();
-    EditText TxtHost = (EditText) findViewById(R.id.editText_host);
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ProgressBar wait_cursor = (ProgressBar) findViewById(R.id.wait_cursor);
+        wait_cursor.setVisibility(View.INVISIBLE);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -40,18 +42,73 @@ public class MainActivity extends AppCompatActivity {
         final Button button = (Button) findViewById(R.id.button_connect);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                int result = 0;
-                String Host = TxtHost.getText().toString();
-                Client.SetConnectionType(S7.OP);
-                result = Client.ConnectTo(Host, 0, 2);
-                if(result == 0){
-                    Toast.makeText(MainActivity.this, "OK", Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show();
-                }
+
+                wait_cursor.setVisibility(View.VISIBLE);
+                PlcConnection p = new PlcConnection();
+                p.read = true;
+                new Thread(p).start();
+
+
 
             }
         });
+    }
+
+
+    private class PlcConnection implements Runnable{
+        private final S7Client Client;
+        public boolean read=false;
+        public boolean write=false;
+
+        public PlcConnection(){
+            Client = new S7Client();
+        }
+
+        public void run() {
+            String IpAddr = "192.168.1.10";
+            int LocTSAP = 0;
+            int RemTSAP = 0;
+            int selectedAreaRadio = 0;
+            int selectedArea = S7.S7AreaPE;
+            int dBNumber = 0;
+            int offset = 0;
+            int rack = 0;
+            int slot = 2;
+            boolean useTSAP = true;
+            int res = 0;
+            View view = (View) findViewById(R.id.root_view);
+            ProgressBar wait_cursor = (ProgressBar) findViewById(R.id.wait_cursor);
+            //collect connection data
+            try {
+                EditText t = (EditText) findViewById(R.id.editText_host);
+                IpAddr = t.getText().toString();
+
+                //wait_cursor.setVisibility(VISIBLE);
+                Client.SetConnectionType(S7.OP);
+                res = Client.ConnectTo(IpAddr, rack, slot);
+
+
+            }catch (NumberFormatException e) {
+
+                Snackbar.make(view, "System error!", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+
+
+
+            if (res==0){
+                Snackbar.make(view, "Connected to " + IpAddr, Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+
+            }
+            else {
+                Snackbar.make(view, "Connection error: "+ S7Client.ErrorText(res), Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+            }
+
+        }
     }
 
     @Override
